37ffc8c897955c4eb9f33f23793178c2dd035b57
Awe1alex <aleksejs.birjulins@gmail.com>
2022-05-25 01:46:13 +0300

    added image switching and audio

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-const elements = document.querySelectorAll('[data-feature-title]')
-const elementsArray = Array.from(elements)
+const titles = document.querySelectorAll('[data-feature-title]')
+const images = document.querySelectorAll('[data-feature-image]')
+const titlesArray = Array.from(titles)
 
 document.addEventListener('scroll', () => {
-  const activeElement = elementsArray.find(el => {
+  const activeElement = titlesArray.find(el => {
     const rect = el.getBoundingClientRect()
     return rect.top > 250 && rect.top < 600
   })
 
   if (activeElement === undefined) {
-    if (elementsArray[elementsArray.length - 1].getBoundingClientRect().top < 300) {
-      setActive(elementsArray[elementsArray.length - 1].dataset.featureTitle)
+    if (titlesArray[titlesArray.length - 1].getBoundingClientRect().top < 300) {
+      setActive(titlesArray[titlesArray.length - 1].dataset.featureTitle)
     } else {
-      setActive(elementsArray[0].dataset.featureTitle)
+      setActive(titlesArray[0].dataset.featureTitle)
     }
   } else {
     setActive(activeElement.dataset.featureTitle)
   }
 })
 
+let prevActive
+
 const setActive = active => {
-  elements.forEach(el => {
+  if (prevActive !== active) {
+    const switchSound = new Audio('./audio/blurp.mp3')
+    switchSound.play().catch(() => {
+      console.log('autoplay prevented')
+    })
+  }
+
+  titles.forEach(el => {
     if (el.dataset.featureTitle === active) {
       el.classList.add('active')
     } else {
       el.classList.remove('active')
     }
   })
+  images.forEach(el => {
+    if (el.dataset.featureImage === active) {
+      el.classList.add('active')
+    } else {
+      el.classList.remove('active')
+    }
+  })
+  prevActive = active
 }
